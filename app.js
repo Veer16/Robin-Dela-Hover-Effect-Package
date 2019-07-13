43d61399f7760e8817014cf6e97ed819d9786447
Veer16 <veer.mailer@gmail.com>
2019-07-13 17:21:04 +1200

    Dev Ed tutorial for the Robin Dela - Hover effect package

---
+++ app.js
@@ -0,0 +1,10 @@
+new hoverEffect({
+    parent: document.querySelector('.distortion'),
+    intensity: 0.1,
+    image1: './bw1.jpg',
+    image2: './bw2.jpg',
+    angle: Math.PI / 8,
+    speedIn: 2,
+    speedOut: 3,
+    displacementImage: './diss.png'
+});
